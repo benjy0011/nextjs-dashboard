417ba1926267cb26c42c78cd424d4641f7dcc5ce
Benjy <benjyyeoh.wx@ynytechnology.com.my>
2025-04-21 17:24:45 +0800

    Chapter 1 - 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana, Pixelify_Sans } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const pixelifySans = Pixelify_Sans({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
